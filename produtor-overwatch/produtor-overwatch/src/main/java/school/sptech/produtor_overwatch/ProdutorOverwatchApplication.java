57a8bb82c676d9463be0baf9f7203c43994a44e1
VitorHideo <vitor.frutuoso@sptech.school>
2025-10-02 12:57:31 -0300

    Projeto Produtor - Overwatch

---
+++ produtor-overwatch/produtor-overwatch/src/main/java/school/sptech/produtor_overwatch/ProdutorOverwatchApplication.java
@@ -0,0 +1,13 @@
+package school.sptech.produtor_overwatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdutorOverwatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdutorOverwatchApplication.class, args);
+	}
+
+}
